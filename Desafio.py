3fd4295587faac55321bd99fde166c544bc6edf6
André Silva Mendes <andre.smendes.20231@poli.ufrj.br>
2023-12-14 14:38:00 -0300

    commit 3

--- Desafio.py
+++ Desafio.py
@@ -1,50 +1,71 @@
-import pandas as pd
-
-test = pd.read_csv("test.csv")
-train= pd.read_csv("train.csv")
-test_ids = test["PassengerId"]
-train.head(5)
-
-def clean(tenta):
-    x = tenta.drop(["Ticket","Cabin", "Name", "PassengerId"], axis=1)
-    colunas = ["SibSp", "Parch" , "Fare" , "Age"]
-
-    for coluna in colunas:
-        x[coluna].fillna(x[coluna].median(), inplace=True)
-
-    x.Embarked.fillna("U", inplace=True)
-    return x
-
-train= clean(train)
-test=clean(test)
-train.head(5)
-
-train['Sex'] = train['Sex'].map({'female':0, 'male':1})
-train['Embarked'] = train['Embarked'].map({'S':0, 'C':1, 'Q':2, '':0})
-train = train.fillna(0)
-train.head(5)
-test['Sex'] = test['Sex'].map({'female':0, 'male':1})
-test['Embarked'] = test['Embarked'].map({'S':0, 'C':1, 'Q':2, '':0})
-test = test.fillna(0)
-a= train.isnull().sum()
-print(a)
-
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-
-Y = train["Survived"]
-X = train.drop("Survived", axis=1)
-
-X_train, X_val, y_train, Y_val = train_test_split(X, Y, test_size=0.2, random_state=42)
-
-clf= LogisticRegression(random_state=0, max_iter=1000).fit(X_train, y_train)
-predictions = clf.predict(X_val)
 from sklearn.metrics import accuracy_score
-a= accuracy_score(Y_val, predictions)
-print(a)
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
+import numpy as np
 
-submission_preds = clf.predict(test)
+test = pd.read_csv("CSV'S/test.csv")
+train = pd.read_csv("CSV'S/train.csv")
+test_ids = test["PassengerId"]
+train.head()
 
-df= pd.DataFrame({"PassengerId": test_ids, 
+df1 = train.drop(['Name', "PassengerId", 'Ticket', 'Cabin'], axis=1)
+testData = test.drop(['Name', "PassengerId", 'Ticket', 'Cabin'], axis=1)
+testData.head()
+
+# Convertendo strings para valores númericos
+df1['Sex'] = df1['Sex'].map({'female': 0, 'male': 1})
+df1['Embarked'] = df1['Embarked'].map({'S': 0, 'C': 1, 'Q': 2, 'nan': 'NaN'})
+testData['Sex'] = testData['Sex'].map({'female': 0, 'male': 1})
+testData['Embarked'] = testData['Embarked'].map(
+    {'S': 0, 'C': 1, 'Q': 2, 'nan': 'NaN'})
+
+df1.isnull().sum()
+
+testData.isnull().sum()
+
+# Variaveis para armazenar a idade média de cada sexo
+idadeMediaHomem0 = df1[df1['Sex'] == 1]['Age'].median()
+idadeMediaMulher0 = df1[df1['Sex'] == 0]['Age'].median()
+# Atribui o valor de idade média aos passageiros que possuem a idade como nula
+df1.loc[(df1.Age.isnull()) & (df1['Sex'] == 1), 'Age'] = idadeMediaHomem0
+df1.loc[(df1.Age.isnull()) & (df1['Sex'] == 0), 'Age'] = idadeMediaMulher0
+idadeMediaHomem1 = testData[testData['Sex'] == 1]['Age'].median()
+idadeMediaMulher1 = testData[testData['Sex'] == 0]['Age'].median()
+testData.loc[(testData.Age.isnull()) & (
+    testData['Sex'] == 1), 'Age'] = idadeMediaHomem1
+testData.loc[(testData.Age.isnull()) & (
+    testData['Sex'] == 0), 'Age'] = idadeMediaMulher1
+
+df1.isnull().sum()
+
+# verificando quais são os dois passassageiros com embarked nulo
+EmbarkedNulo = df1[df1.Embarked.isnull()]
+EmbarkedNulo
+
+# Atribuindo um valor
+df1 = df1.fillna(2)
+
+# Preenchendo valores nulos na tarifa com a mediana
+tarifaMedia = testData['Fare'].median()
+testData.loc[testData.Fare.isnull(), 'Fare'] = tarifaMedia
+
+df1.isnull().sum()
+
+
+Y = df1["Survived"]
+X = df1.drop("Survived", axis=1)
+
+X_train, X_val, y_train, Y_val = train_test_split(
+    X, Y, test_size=0.2, random_state=42)
+
+clf = LogisticRegression(random_state=0, max_iter=1000).fit(X_train, y_train)
+predictions = clf.predict(X_val)
+a = accuracy_score(Y_val, predictions)
+print(a*100)
+
+submission_preds = clf.predict(testData)
+
+df = pd.DataFrame({"PassengerId": test_ids,
                   "Survived": submission_preds})
 df.to_csv("submission.csv", index=False)
